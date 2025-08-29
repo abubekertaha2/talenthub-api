d52b94b9ae43fa2846cccea1825fcf6532f76e44
Abubeker Taha <abubekertaha3@gmail.com>
2025-08-29 15:25:15 +0300

    Final project files with corrected README

--- config/db.js
+++ config/db.js
@@ -1,23 +1,4 @@
-// import mysql from 'mysql2/promise';
-// import 'dotenv/config';
 
-// // Create the connection pool for localhost
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   ssl: {
-//     ca: process.env.DB_SSL_CA
-//   }
-// });
-
-// export default pool;
-
-// console.log('Database connection pool created.');
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 import fs from 'fs'; 
