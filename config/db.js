da1f91a318b0f14cd2a6bad44e5dc41cf8719a87
Abubeker Taha <abubekertaha3@gmail.com>
2025-08-29 13:52:59 +0300

    Add SSL certificate and update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,42 @@
+// import mysql from 'mysql2/promise';
+// import 'dotenv/config';
+
+// // Create the connection pool for localhost
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   ssl: {
+//     ca: process.env.DB_SSL_CA
+//   }
+// });
+
+// export default pool;
+
+// console.log('Database connection pool created.');
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
+import fs from 'fs'; 
+import path from 'path';
 
-// Create the connection pool for localhost
+const caCertPath = path.resolve('../certs/aiven_ca.pem');
+
+// Create the connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    ca: process.env.DB_SSL_CA
+    ca: fs.readFileSync(caCertPath)
   }
 });
 
